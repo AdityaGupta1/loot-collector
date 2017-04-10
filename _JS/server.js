70bd6af9c216c77947091f2085cb089032b35c2b
AdityaGupta1 <adityag@gmail.com>
2017-04-10 16:51:16 -0700

    added rudimentary server.js file and roles.asciidoc to define what server and client will do

---
+++ _JS/server.js
@@ -0,0 +1,49 @@
+var WebSocketServer = require('websocket').server;
+var http = require('http');
+var port = 8000;
+var clients = {};
+
+var server = http.createServer(function(request, response) {});
+server.listen(port, function() {});
+console.log("connected on port " + port);
+
+socket = new WebSocketServer({
+    httpServer: server
+});
+
+socket.on('request', function(request) {
+    var connection = request.accept(null, request.origin);
+
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            // assume that message is in JSON format
+            var json = JSON.parse(message.utf8Data);
+
+            switch(json.type) {
+                // connect
+                case "connect":
+                    clients[json.text] = connection;
+                    console.log("clients: " + clients);
+                    break;
+                // message
+                case "message":
+                    for (var username in clients) {
+                        clients[username].send(JSON.stringify(json));
+                    }
+                    break;
+                // unknown
+                default:
+                    console.log("what is this m8 this is not a recognized type");
+                    break;
+            }
+
+
+            console.log(request.origin + ", " + message.utf8Data);
+            connection.send(message.utf8Data);
+        }
+    });
+
+    connection.on('close', function(connection) {
+        console.log("connection closed");
+    });
+});
